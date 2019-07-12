cf2c96ac151f200fb37dd601724124b098aee40d
Aline Pádua <aline.padua.97@gmail.com>
2019-07-12 11:31:57 -0300

    Finalizado
    Inserção de ícones e layout finalizado.

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 let btn_menu  = document.getElementById('btn_menu');
 let menu_left = document.getElementById('menu_left');
-btn_menu.onclick = () => {
-    let margin = menu_left.style.marginLeft;
+btn_menu.addEventListener("click", () => {
+    let margin = getComputedStyle(menu_left).getPropertyValue("margin-left");
     margin == '-300px' ? menu_left.style.marginLeft = '0px' : menu_left.style.marginLeft = '-300px';
-}
+});
